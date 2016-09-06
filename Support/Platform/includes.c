86c554ecf50df40392d5aaa8ed4a0d7f1669879a
Allan Odgaard <git@abetarda.com>
2016-09-06 14:53:12 +0200

    Add the code used to generate Platform.tmLanguage

---
+++ Support/Platform/includes.c
@@ -0,0 +1,14 @@
+#import <CoreFoundation/CoreFoundation.h>
+#import <MacTypes.h>
+#import <stdio.h>
+#import <stdint.h>
+#import <math.h>
+#import <pthread.h>
+#import <dispatch/dispatch.h>
+#import <unistd.h>
+#import <fcntl.h>
+
+int main (int argc, char const* argv[])
+{
+	return 0;
+}
